e472cb583fe2c4a56ed2377689e45fa038235a05
Esmeray Tasgit <estasgit@gmail.com>
2021-02-01 16:59:47 +0100

    Portfolio Navbar Werk

---
+++ Portfolio/EsmerayTasgit/app.js
@@ -0,0 +1,9 @@
+//NAVBAR
+function myFunction() {
+    const x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
