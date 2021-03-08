b721afc53f1ef99151bbe125b35f957ec89dba1a
Esmeray Tasgit <estasgit@gmail.com>
2021-03-08 16:39:13 +0100

    nav on scroll

--- Portfolio/EsmerayTasgit/app.js
+++ Portfolio/EsmerayTasgit/app.js
@@ -1,9 +1,15 @@
 //NAVBAR
 function myFunction() {
-    const x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+  const x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
+}
+
+
+window.onscroll = () => {
+  const nav = document.querySelector('#navbar');
+  if(this.scrollY <= 980) nav.className = ''; else nav.className = 'scroll'; 
+};
