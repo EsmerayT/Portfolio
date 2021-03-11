5c353b99ed6fe62b1f183969294e2707a053abc3
Esmeray Tasgit <estasgit@gmail.com>
2021-03-11 14:05:57 +0100

    if statement-nav color on scroll

--- Portfolio/EsmerayTasgit/app.js
+++ Portfolio/EsmerayTasgit/app.js
@@ -9,7 +9,24 @@ function myFunction() {
 }
 
 
+// window.onscroll = () => {
+//   const nav = document.querySelector('#navbar');
+//   if (this.scrollY <= 1000 || this.scrollY >= 1750) nav.className = ''; else nav.className = 'scroll';
+// };
+
+
+
 window.onscroll = () => {
-  const nav = document.querySelector('#navbar');
-  if(this.scrollY <= 1000 || this.scrollY >= 1700) nav.className = ''; else nav.className = 'scroll'; 
+  const nav = document.querySelector('#navbar'); {
+    if (this.scrollY <= 1000) {
+      nav.className = '';
+    } else if (this.scrollY >= 1750) {
+      nav.className = 'blue';
+    // } else if (this.scrollY >= 2000) {
+    //   nav.className = '';
+    } else {
+      nav.className = 'scroll';
+    }
+  }
 };
+
