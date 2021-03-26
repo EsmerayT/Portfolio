2d93a27f1ff9e0f6d3c93a1b32da936cc82a8a93
Esmeray Tasgit <estasgit@gmail.com>
2021-03-26 10:56:39 +0100

    MyPortfolio

---
+++ Portfolio/app.js
@@ -0,0 +1,97 @@
+// Nav Responsive
+function responsiveNav() {
+  const x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+
+// Nav on scroll
+
+let intViewportWidth = window.innerWidth;
+console.log(intViewportWidth)
+
+let y = window.scrollY
+console.log(scrollY)
+
+window.onscroll = () => {
+  const nav = document.querySelector('#navbar'); {
+    if (window.innerWidth <= 600 && this.scrollY <= 2200 || window.innerWidth > 600 && this.scrollY <= 1000) {
+      nav.className = '';
+    } else if (window.innerWidth <= 600 && this.scrollY <= 2950 || window.innerWidth > 600 && this.scrollY <= 1750) {
+      nav.className = 'gray';
+    } else {
+      nav.className = 'blue';
+    }
+  }
+};
+
+// window.onscroll = () => {
+//   const nav = document.querySelector('#navbar'); {
+//     if (window.innerWidth < 800 && this.scrollY <= 2200) {
+//       nav.className = '';
+//     } else if (window.innerWidth < 800 && this.scrollY >= 1750) {
+//       nav.className = 'gray';
+//     } else if (window.innerWidth < 800 && this.scrollY >= 2000) {
+//       nav.className = '';
+//     } else {
+//       nav.className = '';
+//     }
+//   }
+// };
+
+
+
+// window.onscroll = () => {
+//   const nav = document.querySelector('#navbar'); {
+//     if (this.scrollY >= 0 && this.scrollY <= 1000 || this.scrollY >= 1000 && this.scrollY <= 2200  ) {
+//       nav.className = '';
+//     } else if (this.scrollY >= 1750) {
+//       nav.className = 'blue';
+//     } else if (this.scrollY >= 2000) {
+//       nav.className = '';
+//     } else {
+//       nav.className = 'gray';
+//     }
+//   }
+// };
+
+
+// window.onscroll = () => {
+//   const werk = document.getElementById("werk"); 
+//   const bio = document.getElementById("Bio");
+//   const contact = document.getElementById("Contact");{
+//     if (werk.className === "werk") {
+//       nav.className = '';
+//     } else if (bio.className === "Bio") {
+//       nav.className = 'blue';
+//     } else if (contact.className === "Contact") {
+//       nav.className = '';
+//     } else {
+//       nav.className = 'gray';
+//     }
+//   }
+// };
+
+
+// if ($(window).width() < 960) {
+//   alert('Less than 960');
+// }
+// else {
+//   alert('More than 960');
+// }
+
+
+// const mq = window.matchMedia( "(min-width: 960px)" );
+
+// if (mq.matches) {
+//        alert("window width >= 960px");
+// } else {
+//      alert("window width < 960px");
+// }
+
+
+
