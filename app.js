1f9dfe78809df6ab8aa692927b057a22375e5f37
Esmeray Tasgit <estasgit@gmail.com>
2021-04-05 13:19:37 +0200

    responsive small

--- app.js
+++ app.js
@@ -34,9 +34,9 @@ console.log(y)
 
 window.onscroll = () => {
   const nav = document.querySelector('#navbar'); {
-    if (window.innerWidth < 992 && this.scrollY <= 4300 || window.innerWidth > 992 && this.scrollY <= 1000 || window.innerWidth > 1312 && this.scrollY <= 2000) {
+    if (window.innerWidth < 992 && this.scrollY <= 4200 || window.innerWidth > 992 && this.scrollY <= 1000 || window.innerWidth > 1312 && this.scrollY <= 2000) {
       nav.className = '';
-    } else if (window.innerWidth < 992 && this.scrollY <= 6000 || window.innerWidth > 992 && this.scrollY <= 1750 || window.innerWidth > 1312 && this.scrollY <= 3600) {
+    } else if (window.innerWidth < 992 && this.scrollY <= 5500 || window.innerWidth > 992 && this.scrollY <= 1750 || window.innerWidth > 1312 && this.scrollY <= 3600) {
       nav.className = 'gray';
     } else {
       nav.className = 'blue';
