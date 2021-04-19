2f1e71c9870c3a7b3a337e0099a317292c0e11ab
Esmeray Tasgit <estasgit@gmail.com>
2021-04-19 16:04:14 +0200

    media 500px

--- app.js
+++ app.js
@@ -17,11 +17,26 @@ let y = window.scrollY
 console.log(y)
 
 
+// window.onscroll = () => {
+//   const nav = document.querySelector('#navbar'); {
+//     if (window.innerWidth < 992 && this.scrollY <= 4200 || window.innerWidth > 992 && this.scrollY <= 1000 || window.innerWidth > 1312 && this.scrollY <= 2000) {
+//       nav.className = '';
+//     } else if (window.innerWidth < 992 && this.scrollY <= 5900 || window.innerWidth > 992 && this.scrollY <= 1750 || window.innerWidth > 1312 && this.scrollY <= 3600) {
+//       nav.className = 'gray';
+//     } else {
+//       nav.className = 'blue';
+//     }
+//   }
+// };
+
+
+
+
 window.onscroll = () => {
   const nav = document.querySelector('#navbar'); {
-    if (window.innerWidth < 992 && this.scrollY <= 4200 || window.innerWidth > 992 && this.scrollY <= 1000 || window.innerWidth > 1312 && this.scrollY <= 2000) {
+    if (window.innerWidth < 500 && this.scrollY <= 2200 || window.innerWidth > 992 && this.scrollY <= 1000 || window.innerWidth > 1312 && this.scrollY <= 2000) {
       nav.className = '';
-    } else if (window.innerWidth < 992 && this.scrollY <= 5900 || window.innerWidth > 992 && this.scrollY <= 1750 || window.innerWidth > 1312 && this.scrollY <= 3600) {
+    } else if (window.innerWidth < 500 && this.scrollY <= 3200 || window.innerWidth > 992 && this.scrollY <= 1750 || window.innerWidth > 1312 && this.scrollY <= 3600) {
       nav.className = 'gray';
     } else {
       nav.className = 'blue';
