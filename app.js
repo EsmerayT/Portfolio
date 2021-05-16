7a7d0ca0f0ef2a540b3dcb948f07c3482bb8e815
Esmeray Tasgit <estasgit@gmail.com>
2021-05-17 00:50:16 +0200

    1900px

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ window.onscroll = () => {
   const nav = document.querySelector('#navbar'); {
     if (window.innerWidth < 688 && this.scrollY <= 2200 || window.innerWidth > 900 && this.scrollY <= 1000 || window.innerWidth >= 1900 && this.scrollY <= 1500 || window.innerWidth > 2000 && this.scrollY <= 2000) {
       nav.className = '';
-    } else if (window.innerWidth < 688 && this.scrollY <= 3200 || window.innerWidth > 900 && this.scrollY <= 1750 || window.innerWidth >= 1900 && this.scrollY <= 2600  || window.innerWidth > 2000 && this.scrollY <= 3600) {
+    } else if (window.innerWidth < 688 && this.scrollY <= 3200 || window.innerWidth > 900 && this.scrollY <= 1750 || window.innerWidth >= 1900 && this.scrollY <= 2500  || window.innerWidth > 2000 && this.scrollY <= 3600) {
       nav.className = 'gray';
     } else {
       nav.className = 'blue';
