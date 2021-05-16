0bbd0853e81cd6fff5f6d367adee12c75c399394
Esmeray Tasgit <estasgit@gmail.com>
2021-05-16 23:28:32 +0200

    clean

--- app.js
+++ app.js
@@ -8,28 +8,12 @@ function responsiveNav() {
   }
 }
 
-
 // NAV ON SCROLL
 let intViewportWidth = window.innerWidth;
 console.log(intViewportWidth)
 
 let y = window.scrollY
 console.log(y)
-
-
-// window.onscroll = () => {
-//   const nav = document.querySelector('#navbar'); {
-//     if (window.innerWidth < 992 && this.scrollY <= 4200 || window.innerWidth > 992 && this.scrollY <= 1000 || window.innerWidth > 1312 && this.scrollY <= 2000) {
-//       nav.className = '';
-//     } else if (window.innerWidth < 992 && this.scrollY <= 5900 || window.innerWidth > 992 && this.scrollY <= 1750 || window.innerWidth > 1312 && this.scrollY <= 3600) {
-//       nav.className = 'gray';
-//     } else {
-//       nav.className = 'blue';
-//     }
-//   }
-// };
-
-
 
 
 window.onscroll = () => {
